694788cf5b1426a94d82430c9945f5d3d322fa18
Xiumin Lin <xiumin.lin@laposte.net>
2020-12-18 19:40:44 +0100

    Add first algorithm : naivety

--- nqueen_solving.py
+++ nqueen_solving.py
@@ -1,10 +1,47 @@
-from utils_functions import print_board, can_t_attack, is_soluce
+from utils_functions import *
 
 # Premier algorithme : la naiveté
-def solve_n_queen_small(board_size, board):
-    solved = False
-    return board, solved
+def solve_n_queen_small(board_size, empty_board):
+    for firstY in range(board_size):
+        
+        board = [[0 for x in range(board_size)] for y in range(board_size)] # on reset le board à 0
+        board[firstY][0] = Queen.value
+        nbQueenTotal = 1
 
+        board, solved = backtrack(0, board_size, board, nbQueenTotal)
+        if(solved):
+            return board, True
+
+    return board, False
+
+# Test s'il est possible de placé une reine sur la colonne previous_x + 1
+# _ Appele récursivement la fonction backtrack() si une reine peut etre placé
+#   mais que tous les reines n'ont pas été placées
+# _ Return le board et True si le tout les reinse ont été placées
+# _ Return le board et False si le tout les reinse ont été placées et/ou aucune reine a pu etre placé sur la colonne
+def backtrack(previous_x, board_size, previous_board, nbQueenTotal):
+    current_x = previous_x + 1
+    board = previous_board
+
+    if(current_x >= board_size):
+        return board, False
+    
+    for y in range(board_size):
+        board = [row[:] for row in previous_board] # on copie du board original
+        queen = Queen(y, current_x, board_size, board)
+
+        if(queen.can_be_placed()):
+            board[y][current_x] = Queen.value
+            nbQueenTotal += 1
+            if(nbQueenTotal == board_size):
+                return board, True
+
+            new_board, solved = backtrack(current_x, board_size, board, nbQueenTotal)
+            if(solved):
+                return new_board, solved
+            nbQueenTotal -= 1
+            board[y][current_x] = "0"
+    return board, False
 
 # Deuxième algorithme : le meilleur
 def solve_n_queen_big(board_size, board):
--- queen_functions.py
+++ queen_functions.py
@@ -1,41 +1,40 @@
 class Queen:
     value = 1   # la valeur qui représente la reine dans l'échiquier 
     def __init__(self, y, x, board_size, board):
-        assert board[y][x] == Queen.value
-        self.y = y  # l'indice de la ligne
-        self.x = x  # l'indice de la colonne
+        self.y = y  # l'indice ligne de la reine
+        self.x = x  # l'indice colonne de la reine
         self.board_size = board_size
         self.board = board
 
     # Une fonction qui vérifie si la reine peut attaquer une autre reine
+    # Attention, cette fonction ne vérifie que les cases à droite, en bas, en diagonal bas gauche et droit de la reine
+    # Utilisez cette fonction avec une boucle qui parcourt le board de gauche à droite et puis de haut en bas
     def can_attack(self):
         if (self.can_attack_right() 
                 or self.can_attack_down() 
-                or self.can_attack_diag_left() 
-                or self.can_attack_diag_right()):
+                or self.can_attack_diag_down_left() 
+                or self.can_attack_diag_down_right()):
             return True
         return False
 
 
     # Une fonction qui vérifie si la reine peut attaquer (une autre reine) à sa droite
-    def can_attack_right(self): #Steven
+    def can_attack_right(self):
         for i in range(self.x + 1, self.board_size):
             if (self.board[self.y][i] == Queen.value):
-                print(str(self.y) + " " + str(i))
                 return True
         return False
 
     # Une fonction qui vérifie si la reine peut attaquer en bas
-    def can_attack_down(self): #Steven
+    def can_attack_down(self):
         for i in range(self.y + 1, self.board_size):
-            if (self.board[i][self.x] == Queen.value): 
-                print(str(i) + " " + str(self.x))
+            if (self.board[i][self.x] == Queen.value):
                 return True
         return False
          
 
-    # Une fonction qui vérifie si la reine peut attaquer à sa diagonale gauche
-    def can_attack_diag_left(self): #Steven
+    # Une fonction qui vérifie si la reine peut attaquer à sa diagonale bas gauche
+    def can_attack_diag_down_left(self):
         i = 1
         while((self.board_size - (self.y + i)) > 0 and (self.x - i) >= 0):
             newY = self.y + i
@@ -46,12 +45,41 @@ class Queen:
         return False
 
 
-    # Une fonction qui vérifie si la reine peut attaquer à sa diagonale droite
-    def can_attack_diag_right(self): #Steven
+    # Une fonction qui vérifie si la reine peut attaquer à sa diagonale bas droite
+    def can_attack_diag_down_right(self):
         i = 1
         while((self.board_size - (self.y + i)) > 0 and (self.board_size - (self.x + i)) > 0):
             newY = self.y + i
             newX = self.x + i
+            if (self.board[newY][newX] == Queen.value):
+                return True
+            i += 1
+        return False
+
+    # Une fonction qui vérifie si l'emplacement de la reine n'est pas attaqué
+    # Similaire à la function can_attack(self)
+    # Attention, cette fonction ne vérifie que les cases à gauche, en diagonal haut gauche et en diagonal bas gauche de la reine
+    # Utilisez cette fonction avec une boucle qui parcourt le board de haut en bas et puis de gauche à droite
+    def can_be_placed(self):
+        if (self.can_attack_left()
+                or self.can_attack_diag_up_left() 
+                or self.can_attack_diag_down_left()):
+            return False
+        return True
+
+    # Une fonction qui vérifie si la reine peut attaquer (une autre reine) à sa gauche
+    def can_attack_left(self):
+        for i in range(self.x):
+            if (self.board[self.y][i] == Queen.value):
+                return True
+        return False
+
+    # Une fonction qui vérifie si la reine peut attaquer à sa diagonale haut gauche
+    def can_attack_diag_up_left(self):
+        i = 1
+        while((self.y - i) >= 0 and (self.x - i) >= 0):
+            newY = self.y - i
+            newX = self.x - i
             if (self.board[newY][newX] == Queen.value):
                 return True
             i += 1
